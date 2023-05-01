a7982a26c0bc33048dd09c9b4445ed020567952d
Jdel-Campos <campos.34pss@gmail.com>
2023-05-01 18:54:36 -0300

    feat: update of the app page and creation of logic in the routers folder

---
+++ src/routes/routes.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "../pages/HomePage/home";
+import { LoginPage } from "../pages/LoginPage/login";
+import { RegisterPage } from "../pages/RegisterPage/register";
+import { ErrorPage } from "../pages/ErrorPage/error";
+
+const Router = () => {
+    return(
+        <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/login" element={<LoginPage />}/>
+            <Route path="/register" element={<RegisterPage/>}/>
+
+            <Route path="*" element={<ErrorPage />}/>
+        </Routes>
+    )
+}
